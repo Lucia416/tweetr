2293819859fbb695a0b20718861a4d6f39234a2e
Lucia416 <lucia.awang416@gmail.com>
2017-02-23 23:50:58 +0000

    better now

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,60 +1,4 @@
-// /*
-//  * Client-side JS logic goes here
-//  * jQuery is already loaded
-//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-//  */
-//
-//  var data = [
-//    {
-//      "user": {
-//        "name": "Newton",
-//        "avatars": {
-//          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//        },
-//        "handle": "@SirIsaac"
-//      },
-//      "content": {
-//        "text": "It's raining apples!"
-//      },
-//      "created_at": 1461116232227
-//    },
-//    {
-//      "user": {
-//        "name": "Descartes",
-//        "avatars": {
-//          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//        },
-//        "handle": "@rd" },
-//      "content": {
-//        "text": "Je pense , donc je suis"
-//      },
-//      "created_at": 1461113959088
-//    },
-//    {
-//      "user": {
-//        "name": "Johann von Goethe",
-//        "avatars": {
-//          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//        },
-//        "handle": "@johann49"
-//      },
-//      "content": {
-//        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//      },
-//      "created_at": 1461113796368
-//    }
-//  ];
-
-
-
 $(document).ready(function(){
-  // debugger;
   loadTweets();
   $(".new-tweet form").on('submit', (ev) =>{
     ev.preventDefault();
@@ -66,13 +10,6 @@ $(document).ready(function(){
       alert("Wow, you talk a lot!");
       return;
     }
-
-
-
-    // const data_obj = {}  ;
-    // $('.new-tweet form').serializeArray().forEach((elm) => {
-    //   data_obj[elm.name] = elm.value;
-    // });
 
     $.ajax({
       method:'POST',
@@ -169,11 +106,3 @@ const loadTweets = () => {
    .done(renderTweets)
   .fail(console.error);
 }
-
-
-
-// function createTweetElement(tweet) {
-//   var $tweet = $('<article>').addClass('tweet');
-//   // ...
-//   return $tweet;
-// }
